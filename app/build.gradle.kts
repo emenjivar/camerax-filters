1d6d5f735d39e5f9ae6a170a895d8f9cb6fecb6e
carlos menjivar <carlosmenjivar@ravn.co>
2023-09-10 13:46:10 -0600

    feat: Request camera permissions on runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     implementation("androidx.camera:camera-view:${cameraxVersion}")
     implementation("androidx.camera:camera-extensions:${cameraxVersion}")
 
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/emenjivar/camerafilter/screen/camera/CameraScreen.kt
+++ app/src/main/java/com/emenjivar/camerafilter/screen/camera/CameraScreen.kt
@@ -1,5 +1,7 @@
 package com.emenjivar.camerafilter.screen.camera
 
+import android.Manifest
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -10,13 +12,33 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun CameraScreen() {
+
+    val permissionState = rememberPermissionState(
+        permission = Manifest.permission.CAMERA,
+        onPermissionResult = { isGranted ->
+            Log.wtf("CameraScreen", "camera permission granted: $isGranted")
+        }
+    )
+
+    LaunchedEffect(Unit) {
+        if (permissionState.status.isGranted) {
+            // Start here the camera
+        } else {
+            permissionState.launchPermissionRequest()
+        }
+    }
+
     Scaffold { paddingValues ->
         Box(
             modifier = Modifier
